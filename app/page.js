c5be2e98f04811fb50b17b4e2424e391602f2d51
Jisun696 <istiakkashem35@gmail.com>
2025-09-15 04:00:31 +0600

    move public folder and add parallel route slots(skeleton) and add in the layout

--- app/page.js
+++ app/page.js
@@ -1,3 +1,3 @@
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">HELLO WORLD</h1>;
+  return <h1 className="text-3xl font-bold underline">HELLO WORLD, HOME PAGE</h1>;
 }
