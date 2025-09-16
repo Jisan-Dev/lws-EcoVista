aa30d5bfdb6ae3518f1ce4a6d26e5e803d6d8698
Jisun696 <istiakkashem35@gmail.com>
2025-09-17 03:37:04 +0600

    build the aqi component card
    make the weather and location api calls in separate file (lib folder) making it like a library to get the data and call them in the components like did in the just built AQIComponent

--- app/[location]/@aqi/page.js
+++ app/[location]/@aqi/page.js
@@ -1,5 +1,7 @@
-const AQIPage = () => {
-  return <div>AQIPage</div>;
+import AQIComponent from "@/components/AQIcomponent";
+
+const AQIPage = ({ params: { location }, searchParams: { latitude, longitude } }) => {
+  return <AQIComponent lat={latitude} lon={longitude} />;
 };
 
 export default AQIPage;
